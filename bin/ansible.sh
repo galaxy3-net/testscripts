a1049db3a4245dea8b2b5f27a835594d0d6c346a
kirscht <korben@kirscht.com>
2023-03-03 20:43:43 -0800

    ansible

--- bin/ansible.sh
+++ bin/ansible.sh
@@ -8,6 +8,16 @@ cat <<_EOD_
 
 _EOD_
 
+wall <<_EOD_
+
+  Installing Ansible
+
+  * * * * * * * * * *
+
+_EOD_
+
+test -e /etc/motd && rm /etc/motd
+
 exec 2>&1
 exec > /tmp/ansibile_install.log
 
@@ -20,5 +30,13 @@ cd /testscripts/
 sudo ansible-playbook -i "localhost," -c local playbooks/playbook.yml
 
 (crontab -l ; echo "@reboot cd /testscripts/ && ansible-playbook -i \"localhost,\" -c local playbooks/playbook.yml")| crontab -
+
+wall <<_EOD_
+
+  Ansible Installation is Complete
+
+  * * * * * * * * * *
+
+_EOD_
 
 rm $0
