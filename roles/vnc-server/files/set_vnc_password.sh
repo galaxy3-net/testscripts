4c7e71c5de0c2775983616e6c61a2d9bf11c34e9
kirscht <korben@kirscht.com>
2023-03-03 19:50:16 -0800

    ansible

--- roles/vnc-server/files/set_vnc_password.sh
+++ roles/vnc-server/files/set_vnc_password.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-mypasswd="${1}"
-mypasswd="uci2020!"
+myuser="${1}"
+mypasswd="${2:=uci2020!}"
 
-echo "${mypasswd}" | vncpasswd -f > /home/vagrant/.vnc/passwd
+sudo -u "${myuser}" echo "${mypasswd}" | vncpasswd -f > /home/vagrant/.vnc/passwd
 
-chown vagrant: /home/vagrant/.vnc/passwd
-chmod 0600 /home/vagrant/.vnc/passwd
+#chown vagrant: /home/vagrant/.vnc/passwd
+#chmod 0600 /home/vagrant/.vnc/passwd
